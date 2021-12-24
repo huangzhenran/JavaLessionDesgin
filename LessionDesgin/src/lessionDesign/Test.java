58ac5edb8b9739165631eb16c28f5e83cfc09ac9
huangzhenran <1214084237@qq.com>
2021-12-24 17:55:18 +0800

    登陆界面已经做好。

--- LessionDesgin/src/lessionDesign/Test.java
+++ LessionDesgin/src/lessionDesign/Test.java
@@ -4,8 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       login login=new login();
-       login.createWindow();
+   //  login login=new login();
+		
+       logintest tLogintest=new logintest();
+       
 	}
 
 }
--- LessionDesgin/src/lessionDesign/login.java
+++ LessionDesgin/src/lessionDesign/login.java
@@ -1,24 +1,66 @@
 package lessionDesign;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.util.jar.JarInputStream;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.sun.javafx.geom.AreaOp.AddOp;
 
 public class login {
 
-	public void createWindow() {
-		 JFrame jf=new JFrame("窗口1");
-	     jf.setSize(1200,800);
-	     jf.setLayout(new FlowLayout());
-	     jf.setLocation(350, 150);
-	     jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public login() {
+		 JFrame jf=new JFrame("窗口2");
+		 JTextField account,password;
+		 
+		 account=new JTextField(20);
+	    password=new JTextField(20);
 	     
-	     JPanel jPanelTop=new JPanel(); //创建面板
-	     JPanel jPanelButtom=new JPanel();
-	     JPanel jPanelCenter=new JPanel();
-	     jPanelTop.setBackground(Color.gray);
+	    JLabel j1=new JLabel("帐号:");
+	    JLabel j2=new JLabel("密码:");
+	    j1.setForeground(Color.white);
+	    j2.setForeground(Color.white);
+	    Font font = new Font("宋体", Font.PLAIN, 25);//创建1个字体实例
+	    j1.setFont(font);
+	    j2.setFont(font);
+	    
+	     JPanel jp1=new JPanel(); //创建面板
+	     JPanel jp2=new JPanel();
+	     JPanel jp3=new JPanel();
+	     
+	     jp1.add(j1);
+	     jp1.add(account);
+	     jp2.add(j2);
+	     jp2.add(password);
+	     
+	   
+	     
+	     JLabel label=new JLabel();
+	        ImageIcon icon=new ImageIcon("image\\nami.jpg");
+	        Image image=icon.getImage();
+	        image=image.getScaledInstance(1200, 800, image.SCALE_DEFAULT);
+	        icon.setImage(image);
+	        label.setIcon(icon);
+	        
+	       // jf.add(label);
+	        jf.setLayout(new GridLayout(2,1));
+	        jf.add(jp1);
+	        jf.add(jp2);
+	        
+	        jf.setSize(1200,800);
+		     jf.setLayout(new FlowLayout());
+		     jf.setLocation(350, 150);
+		     jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	     jf.setVisible(true);
 	}
 }
